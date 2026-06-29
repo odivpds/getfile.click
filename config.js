4334377cb6d91be7091a0b4c8ecf9c2e534f2ed3
agusprana <odhivpds01@gmail.com>
2026-06-29 23:51:32 +0800

    fix iklan pop under

--- config.js
+++ config.js
@@ -3,12 +3,9 @@
  * File ini TIDAK AKAN DIACAK, sehingga Client Anda bisa mengedit ini di Github.
  */
 const CONFIG = {
-    // URL API Pusat (Server Utama Anda)
+    // URL API Pusat (Server Utama Anda) : API_BASE_URL: 'https://api.getfiles.my.id',
     API_BASE_URL: 'https://api.getfiles.my.id',
 
-    // URL Direct Link Pop-under Milik Client (Monetag, dsb)
-    // CLIENT_POPUNDER_URL: 'https://www.google.com/search?q=iklan-popunder-client',
-
-    // Script Iklan (Vignette / OnClick Monetag dll)
-    MONETAG_SCRIPT: `<script>(function(s){s.dataset.zone='11208851',s.src='https://llvpn.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>`
+    // URL Direct Link Pop-under Milik Client (Kosongkan jika menggunakan script di index.html)
+    CLIENT_POPUNDER_URL: ''
 };
